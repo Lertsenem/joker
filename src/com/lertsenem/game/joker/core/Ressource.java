02dc48b52f60a0e245e84eb3a6908103f753bc74
Lertsenem <lertsenem@yahoo.fr>
2013-02-21 00:22:39 +0100

    Ajout de l'interface Ressource

---
+++ src/com/lertsenem/game/joker/core/Ressource.java
@@ -0,0 +1,11 @@
+/**
+ * The <code>Ressource</code> interface describes an object with an <code>Identifiant</code>.
+ */
+public interface Ressource
+{
+	/**
+	 * Get the <code>Identifiant</code> associated withi the <code>Ressource</code>
+	 */
+	public Identifiant getId();
+
+}
